80a3bfb263dce45e8dccbc547cb1dded5c00ad0a
Sourdeval <sourdeval@laposte.net>
2025-06-26 13:17:44 +0200

    [Home] v1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'omnak-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
