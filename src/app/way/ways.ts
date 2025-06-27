3d43cf6984953485cc4f5609cca464db5fd6b9ea
Sourdeval <sourdeval@laposte.net>
2025-06-27 15:58:51 +0200

    [Way] Create empty ways for Lvl 1 & 2

--- src/app/way/ways.ts
+++ src/app/way/ways.ts
@@ -6,11 +6,299 @@ export let ways: Way[] = [
         name: "Soldat",
         description: "Toute l'armée de la Nation Rouge commence par une formation de soldat. Les bases du combat et de la discipline y sont acquises.",
         asset: "Honorable",
-        equipment: "Glaive, cotte de maille",
+        equipment: "Glaive, cotte de mailles",
         skills: [
             { name: "Estoc", difficulty: 'C8', effects: ['1 blessure'] },
             { name: "Diagonale", difficulty: 'C10', effects: ['1 blessure', 'ER4 : 1 blessure'] },
             { name: "Garde", difficulty: 'R12', effects: ['bonus pour se défendre des attaques au corps à corps', 'R : malus à la place'] },
         ]
-    }
+    },
+    {
+        id: "MageFeu",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Cavalier",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "LancierEnchante",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "FantassinEnchante",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "CogneurEnchante",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Cueilleur",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Chasseur",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Soigneur",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Druide",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Changeur",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "Rodeur",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "TireurVenimeux",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "TireurElite",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "MageSable",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "EnfantPoignard",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurObsidienne",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurEmeraude",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurSaphir",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurTopaze",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurRubis",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurPyrite",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurAmethyste",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurHematite",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
+    {
+        id: "PorteurTurquoise",
+        name: "",
+        description: "",
+        asset: "",
+        equipment: "",
+        skills: [
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+            { name: "", difficulty: '', effects: [''] },
+        ]
+    },
 ]
